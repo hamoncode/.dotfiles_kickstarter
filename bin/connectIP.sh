4fbdb168be4596e385a94e810cdf6e645955ba42
hamoncode <vincent.hamon23@gmail.com>
2025-03-25 16:48:27 -0400

    cleaning unnecessary stuff

--- bin/connectIP.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ip addr add 10.0.0.20/24 dev enp3s0 #|| { echo " ip addr failed" ; exit 1 ;
-ip link set enp3s0 || { echo " second ifconfig failed" ; exit 1 ; 
-
-echo "connection réussi"
