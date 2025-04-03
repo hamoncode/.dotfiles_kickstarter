b5dbddc7a7473fe29c7a2033e7d88b361159f882
hamoncode <vincent.hamon23@gmail.com>
2025-04-03 17:11:58 -0400

    donner script

--- bin/createGitRepo.sh
+++ bin/createGitRepo.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
 # Usage: create_repo.sh <repository-name>
-# declaration user et nom de repo
 
+# CONFIGURATION **entrer votre user git**
+github_username=""
+repository_name="$1"
 
 # Initialize Git repository
-
+git init
+git branch -m main
 
 # premier commit
-
+touch README.md
+git add .
+git commit -m "Initial commit" 
 
 # Creer repo sur GitHub
-
+gh repo create "$github_username/$repository_name" --private --confirm 
 
 # ajouter GitHub comme remote avec https
-
+git remote add origin "https://github.com/$github_username/$repository_name.git"
 
 # Push dans GitHub
-
+git push -u origin main 
 
 # message de reussite
+echo "operation reussi"
+
